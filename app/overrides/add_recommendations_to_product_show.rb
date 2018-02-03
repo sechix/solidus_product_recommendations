d032ce1459e6acfc7310d2d4ef960e5b196a5b66
Maria Jose <sechix@gmail.com>
2018-02-03 01:47:25 +0100

    override change

--- app/overrides/add_recommendations_to_product_show.rb
+++ app/overrides/add_recommendations_to_product_show.rb
@@ -1,6 +1,6 @@
 Deface::Override.new(:virtual_path => "spree/products/show",
                      :name => "product_recommendations",
-                     :insert_bottom => "[data-hook='promotions']",
+                     :insert_bottom => "[data-hook='product-properties']",
                      :partial => "spree/products/product_recommendations",
                      :disabled => false,
                      :namespaced => true)
